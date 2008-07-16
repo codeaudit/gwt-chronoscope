eef17f0d03052902447d83d4f43d81c233e40129
chadtakahashi <chadtakahashi@5cd50543-e33d-0410-8f10-d7e7bb9d3266>
2008-07-16 20:27:29 +0000

    -  Removed an unused method and some dead comments in DatasetLegendPanel

--- chronoscope/src/main/java/org/timepedia/chronoscope/client/render/DatasetLegendPanel.java
+++ chronoscope/src/main/java/org/timepedia/chronoscope/client/render/DatasetLegendPanel.java
@@ -73,34 +73,6 @@ public class DatasetLegendPanel extends AbstractPanel {
     }
   }
   
-  private void calcDimensions(Layer layer) {
-    updateHoverInfo();
-    
-    double xCursor = this.x;
-    double yCursor = this.y;
-    
-    for (int i = 0; i < plot.getSeriesCount(); i++) {
-      //TODO: Need method that just returns width of legend label
-      double lblWidth = drawLegendLabel(xCursor, yCursor, layer, i, false);
-      boolean enoughRoomInCurrentRow = (lblWidth >= 0);
-
-      if (enoughRoomInCurrentRow) {
-        xCursor += lblWidth;
-      } else {
-        xCursor = this.x;
-        yCursor += lblHeight;
-
-        //TODO: Need method that just returns width of legend label w/out actually drawing.
-        xCursor += drawLegendLabel(xCursor, yCursor, layer, i, false);
-      }
-      
-      xCursor +=  DATASET_LEGEND_SPACER;
-    }
-    
-    width = xCursor - this.x;
-    height = yCursor - this.y;
-  }
-  
   public void resizeToIdealWidth() {
     throw new UnsupportedOperationException();
   }
@@ -170,39 +142,4 @@ public class DatasetLegendPanel extends AbstractPanel {
     prevHoveredDatasetIdx = hoveredDatasetIdx;
     prevHoveredPointIdx = hoveredPointIdx;
   }
-
-  /*
-  private Bounds getLegendLabelBounds(Layer layer, Bounds axisBounds) {
-    int labelHeight = calcHeight("X", layer);
-
-    Bounds b = new Bounds();
-    double x = axisBounds.x;
-    b.height = axisBounds.y + labelHeight + LEGEND_Y_TOP_PAD;
-
-    for (int i = 0; i < plot.getSeriesCount(); i++) {
-      int labelWidth = calcWidth(plot.getSeriesLabel(i), layer);
-      boolean enoughRoomInCurrentRow = (x + labelWidth) < axisBounds.width;
-      if (enoughRoomInCurrentRow) {
-        x += labelWidth;
-        b.width = Math.max(b.width, x);
-      } else {
-        b.height += labelHeight;
-        x = axisBounds.x + labelWidth;
-      }
-    }
-
-    // Issue #41: For now, we add a LEGEND_Y_BOTTOM_PAD that's tall enough to
-    // allow for the possibility of an extra row of legend labels. This is to
-    // account for the case where the user hovers over a dataset point, causing
-    // the corresponding range value to be appended to the legend label, which
-    // in some cases could cause the remaining legend labels to run over into a
-    // new row.
-    b.height += labelHeight + LEGEND_Y_BOTTOM_PAD;
-
-    b.x = 0;
-    b.y = 0;
-
-    return b;
-  }
-  */
 }
