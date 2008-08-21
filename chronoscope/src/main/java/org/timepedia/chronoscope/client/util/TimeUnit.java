d14e5216c3dfe6a8f70bd4debb4651c9daf2bc67
chadtakahashi <chadtakahashi@5cd50543-e33d-0410-8f10-d7e7bb9d3266>
2008-08-21 23:47:16 +0000

    Classes that were formerly defining their own time unit intervals (e.g. 'HOUR = 1000*60*60') now use the TimeUnit enum (e.g.'TimeUnit.HOUR.ms()').

--- chronoscope/src/main/java/org/timepedia/chronoscope/client/util/TimeUnit.java
+++ chronoscope/src/main/java/org/timepedia/chronoscope/client/util/TimeUnit.java
@@ -9,11 +9,13 @@ public enum TimeUnit {
   MS (1),
   SEC (1000),
   MIN (SEC.ms * 60),
-  HR (MIN.ms * 60),
-  DAY (HR.ms * 24),
-  MONTH (DAY.ms * 365.2425 / 12.0), // avg # days in year is 365.2425
-  YR (MONTH.ms * 12),
-  DECADE (YR.ms * 10),
+  HOUR (MIN.ms * 60),
+  DAY (HOUR.ms * 24),
+  MONTH (DAY.ms * 31.0),
+  YEAR (DAY.ms * 365.2425),
+  //MONTH (DAY.ms * 365.2425 / 12.0), // avg # days in year is 365.2425, 
+  //YR (MONTH.ms * 12),
+  DECADE (YEAR.ms * 10),
   CENTURY (DECADE.ms * 10),
   MILLENIUM (CENTURY.ms * 10);
   
@@ -24,13 +26,13 @@ public enum TimeUnit {
   }
   
   /**
-   * Returns this time interval in milliseconds
+   * Returns this time interval in milliseconds.
    */
   public double ms() {
     return ms;
   }
   
   public static final void main(String[] args) {
-    System.out.println((long)TimeUnit.YR.ms);
+    System.out.println((long)TimeUnit.YEAR.ms);
   }
 }
