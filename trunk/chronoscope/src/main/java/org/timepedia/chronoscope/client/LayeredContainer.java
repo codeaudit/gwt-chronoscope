286aa7d8a1a2669f0bb0dc62e7fc084ea3691ff3
cromwellian <cromwellian@5cd50543-e33d-0410-8f10-d7e7bb9d3266>
2008-08-27 00:24:51 +0000

    InfoWindows now use decorated popup panel, removed override of labels in drawOverlays

---
+++ trunk/chronoscope/src/main/java/org/timepedia/chronoscope/client/LayeredContainer.java
@@ -0,0 +1,13 @@
+package org.timepedia.chronoscope.client;
+
+public class LayeredContainer<S extends Component, T extends Container>
+    extends AbstractContainer<S,T> {
+
+  protected void onComponentAdded(S component) {
+    //To change body of implemented methods use File | Settings | File Templates.
+  }
+
+  protected void onComponentRemoved(S component) {
+    //To change body of implemented methods use File | Settings | File Templates.
+  }
+}
