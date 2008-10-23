70e624684338766a3ea08ff0326bce7449e3ed75
cromwellian <cromwellian@5cd50543-e33d-0410-8f10-d7e7bb9d3266>
2008-10-23 22:52:59 +0000

    Partial fixup of Legend to cope with non-date X values. TODO: delegate more formatting to AxisPanel. Refactor commonality of Range/Domain axis renderers into super class.

--- trunk/chronoscope/src/main/java/org/timepedia/chronoscope/client/render/DateRangePanel.java
+++ trunk/chronoscope/src/main/java/org/timepedia/chronoscope/client/render/DateRangePanel.java
@@ -8,54 +8,78 @@ import org.timepedia.chronoscope.client.util.date.DateFormatHelper;
 
 /**
  * Draws a date range (e.g. "11/25/1995 - 02/17/2007").
- * 
+ *
  * @author Chad Takahashi
  */
 public class DateRangePanel extends AbstractPanel {
+
   private static final String SPC = "\u00A0";
+
   private static final String DATE_DELIM_LONG = SPC + "-" + SPC;
+
   private static final String DATE_DELIM_SHORT = SPC + "-" + SPC;
 
   private static final double SHOW_DAY_THRESHOLD = TimeUnit.DAY.ms() * 28;
+
   private static final double SHOW_MONTH_THRESHOLD = TimeUnit.DAY.ms() * 360;
-  
+
   private static final DateFormatHelper DATE_FMT = new DateFormatHelper();
-  
+
   private String dateRangeActive;
+
   private String dateRangeShort;
+
   private String dateRangeLong;
 
   private double startTimeStamp = -1;
+
   private ChronoDate startDate = ChronoDate.getSystemDate();
+
   private double endTimeStamp = -1;
+
   private ChronoDate endDate = ChronoDate.getSystemDate();
 
   // If true, render the thin date range. Otherwise, use the more
   // verbose date range.
   private boolean compactMode = false;
-  
+
   private boolean doShowDayInDate, doShowMonthInDate;
-  
+
   private int typicalCharWidth;
-  
+
+  private RangeAxisPanel domainAxisRenderer;
+
+  private boolean isDateDomain;
+
   //public void init(Layer layer) {
-  public void init(Layer layer, double minDomainInterval) {
+  public void init(Layer layer, double minDomainInterval, double maxDomain,
+      RangeAxisPanel domainAxisRenderer) {
+    isDateDomain = domainAxisRenderer instanceof DomainAxisPanel;
+
+    this.domainAxisRenderer = domainAxisRenderer;
     doShowDayInDate = minDomainInterval < SHOW_DAY_THRESHOLD;
     doShowMonthInDate = minDomainInterval < SHOW_MONTH_THRESHOLD;
 
     final String typicalDateChars = "0123456789-";
     height = this.calcHeight("X", layer);
-    typicalCharWidth = calcWidth(typicalDateChars, layer) / typicalDateChars.length();
-    
-    if (compactMode) {
-      final String typicalShortDateRange = "12/12/00" + DATE_DELIM_SHORT + "12/12/00";
-      this.width = this.calcWidth(typicalShortDateRange, layer);
-      //resizeToMinimalWidth();
-    }
-    else {
-      final String typicalLongDateRange = "12/12/0000" + DATE_DELIM_LONG + "12/12/0000";
-      this.width = this.calcWidth(typicalLongDateRange, layer);
-      //resizeToIdealWidth();
+    typicalCharWidth = calcWidth(typicalDateChars, layer) / typicalDateChars
+        .length();
+
+    if (isDateDomain) {
+      if (compactMode) {
+        final String typicalShortDateRange = "12/12/00" + DATE_DELIM_SHORT
+            + "12/12/00";
+        this.width = this.calcWidth(typicalShortDateRange, layer);
+        //resizeToMinimalWidth();
+      } else {
+        final String typicalLongDateRange = "12/12/0000" + DATE_DELIM_LONG
+            + "12/12/0000";
+        this.width = this.calcWidth(typicalLongDateRange, layer);
+        //resizeToIdealWidth();
+      }
+    } else {
+      this.width = this
+          .calcWidth(domainAxisRenderer.formatLegendLabel(maxDomain), layer);
     }
   }
 
@@ -73,24 +97,31 @@ public class DateRangePanel extends AbstractPanel {
           + startTimeStamp + ", " + endTimeStamp);
     }
 
-    boolean dateRangeChanged = (startTimeStamp != this.startTimeStamp)
-        || (endTimeStamp != this.endTimeStamp);
+    boolean dateRangeChanged = (startTimeStamp != this.startTimeStamp) || (
+        endTimeStamp != this.endTimeStamp);
+
+    this.startTimeStamp = startTimeStamp;
+    this.endTimeStamp = endTimeStamp;
 
     if (dateRangeChanged) {
-      this.startTimeStamp = startTimeStamp;
-      this.endTimeStamp = endTimeStamp;
+      if (isDateDomain) {
+        startDate.setTime(startTimeStamp);
+        endDate.setTime(endTimeStamp);
 
-      startDate.setTime(startTimeStamp);
-      endDate.setTime(endTimeStamp);
-      
-      String longStartDate = formatLongDate(startDate);
-      String longEndDate = formatLongDate(endDate);
-      dateRangeLong = longStartDate + DATE_DELIM_LONG + longEndDate;
-      String shortStartDate = formatShortDate(startDate);
-      String shortEndDate = formatShortDate(endDate);
-      dateRangeShort = shortStartDate + DATE_DELIM_SHORT + shortEndDate;
+        String longStartDate = formatLongDate(startDate);
+        String longEndDate = formatLongDate(endDate);
+        dateRangeLong = longStartDate + DATE_DELIM_LONG + longEndDate;
+        String shortStartDate = formatShortDate(startDate);
+        String shortEndDate = formatShortDate(endDate);
+        dateRangeShort = shortStartDate + DATE_DELIM_SHORT + shortEndDate;
 
-      dateRangeActive = compactMode ? dateRangeShort : dateRangeLong;
+        dateRangeActive = compactMode ? dateRangeShort : dateRangeLong;
+      } else {
+        dateRangeActive = dateRangeLong = dateRangeShort =
+            domainAxisRenderer.formatLegendLabel(startTimeStamp)
+                + DATE_DELIM_LONG + domainAxisRenderer
+                .formatLegendLabel(endTimeStamp);
+      }
     }
   }
 
@@ -105,23 +136,23 @@ public class DateRangePanel extends AbstractPanel {
     dateRangeActive = dateRangeLong;
     width = estimateStringWidth(dateRangeActive);
   }
-  
+
   private String formatLongDate(ChronoDate d) {
     String mo = doShowMonthInDate ? (DATE_FMT.pad(d.getMonth() + 1) + "/") : "";
     String dy = doShowDayInDate ? (DATE_FMT.pad(d.getDay()) + "/") : "";
     String yr = d.getYear() + "";
-    
+
     return mo + dy + yr;
   }
 
   private String formatShortDate(ChronoDate d) {
-    String mo = doShowMonthInDate ? (DATE_FMT.pad(d.getMonth() + 1) + "/") : ""; 
-    String dy = doShowDayInDate ? (DATE_FMT.pad(d.getDay()) + "/") : ""; 
+    String mo = doShowMonthInDate ? (DATE_FMT.pad(d.getMonth() + 1) + "/") : "";
+    String dy = doShowDayInDate ? (DATE_FMT.pad(d.getDay()) + "/") : "";
     String yr = DATE_FMT.twoDigitYear(d);
 
     return mo + dy + yr;
   }
-  
+
   private int estimateStringWidth(String s) {
     return this.typicalCharWidth * s.length();
   }
--- trunk/chronoscope/src/main/java/org/timepedia/chronoscope/client/render/LegendAxisPanel.java
+++ trunk/chronoscope/src/main/java/org/timepedia/chronoscope/client/render/LegendAxisPanel.java
@@ -1,9 +1,11 @@
 package org.timepedia.chronoscope.client.render;
 
+import org.timepedia.chronoscope.client.XYPlot;
 import org.timepedia.chronoscope.client.canvas.Bounds;
 import org.timepedia.chronoscope.client.canvas.Layer;
 import org.timepedia.chronoscope.client.util.ArgChecker;
 import org.timepedia.chronoscope.client.util.Interval;
+import org.timepedia.chronoscope.client.util.MathUtil;
 import org.timepedia.chronoscope.client.util.TimeUnit;
 
 /**
@@ -12,8 +14,8 @@ import org.timepedia.chronoscope.client.util.TimeUnit;
 public class LegendAxisPanel extends AxisPanel {
 
   /**
-   * Dictates the Y-padding between the top of the legend item bounds
-   * and the bottom of the zoom and date range panels.
+   * Dictates the Y-padding between the top of the legend item bounds and the
+   * bottom of the zoom and date range panels.
    */
   private static final int LEGEND_Y_TOP_PAD = 3;
 
@@ -22,9 +24,9 @@ public class LegendAxisPanel extends AxisPanel {
   private DateRangePanel dateRangePanel;
 
   private DatasetLegendPanel dsLegendPanel;
-  
+
   private ZoomListener zoomListener;
-  
+
   private ZoomPanel zoomPanel;
 
   public boolean click(int x, int y) {
@@ -34,10 +36,10 @@ public class LegendAxisPanel extends AxisPanel {
 
   public void draw(Layer layer, Bounds axisBounds) {
     Interval domainInterval = plot.getDomain();
-    final int labelHeight = (int)this.zoomPanel.height;
+    final int labelHeight = (int) this.zoomPanel.height;
     copyState(axisBounds, bounds);
     clearAxis(layer, axisBounds);
-    
+
     // Position and size the panels
     zoomPanel.setLocation(axisBounds.x, axisBounds.y);
     double startDate = domainInterval.getStart();
@@ -45,14 +47,15 @@ public class LegendAxisPanel extends AxisPanel {
     dateRangePanel.updateDomainInterval(startDate, endDate);
     topRightJustify(dateRangePanel, axisBounds);
     layoutPanels(axisBounds);
-    dsLegendPanel.setLocation(axisBounds.x, axisBounds.y + labelHeight + LEGEND_Y_TOP_PAD);
-    
+    dsLegendPanel.setLocation(axisBounds.x,
+        axisBounds.y + labelHeight + LEGEND_Y_TOP_PAD);
+
     // Draw the panels
     zoomPanel.draw(layer);
     dateRangePanel.draw(layer);
     dsLegendPanel.draw(layer);
   }
-  
+
   /**
    * Returns the total height of the rendered legend axis
    */
@@ -77,38 +80,41 @@ public class LegendAxisPanel extends AxisPanel {
   public double getWidth() {
     return this.view.getWidth();
   }
-  
+
   @Override
   protected void initHook() {
     ArgChecker.isNotNull(plot, "plot");
     ArgChecker.isNotNull(zoomListener, "zoomListener");
-    
-    ZoomIntervals zoomIntervals = createDefaultZoomIntervals();
-    final double approxMinInterval = Math.max(0, plot.getDatasets().getMinInterval());
+
+    ZoomIntervals zoomIntervals = createDefaultZoomIntervals(plot);
+    final double approxMinInterval = Math
+        .max(0, plot.getDatasets().getMinInterval());
     final double minDomain = plot.getDatasets().getMinDomain();
     final double maxDomain = plot.getDatasets().getMaxDomain();
     zoomIntervals.applyFilter(minDomain, maxDomain, approxMinInterval);
-    
+
     Layer rootLayer = view.getCanvas().getRootLayer();
-    
+
     dsLegendPanel = new DatasetLegendPanel();
     dsLegendPanel.setPlot(plot);
     dsLegendPanel.setView(view);
     dsLegendPanel.setGssProperties(labelProperties);
     dsLegendPanel.setTextLayerName(textLayerName);
     dsLegendPanel.init(rootLayer);
-    
+
     zoomPanel = new ZoomPanel();
     zoomPanel.setGssProperties(labelProperties);
     zoomPanel.setTextLayerName(textLayerName);
     zoomPanel.addListener(zoomListener);
     zoomPanel.setZoomIntervals(zoomIntervals);
     zoomPanel.init(rootLayer);
-    
+
     dateRangePanel = new DateRangePanel();
     dateRangePanel.setGssProperties(labelProperties);
     dateRangePanel.setTextLayerName(textLayerName);
-    dateRangePanel.init(rootLayer, plot.getDatasets().getMinInterval());
+    dateRangePanel.init(rootLayer, plot.getDatasets().getMinInterval(),
+        plot.getDatasets().getMaxDomain(),
+        (RangeAxisPanel) plot.getDomainAxisRenderer());
 
     dateRangePanel.updateDomainInterval(minDomain, maxDomain);
 
@@ -118,7 +124,7 @@ public class LegendAxisPanel extends AxisPanel {
   public void setZoomListener(ZoomListener l) {
     this.zoomListener = l;
   }
-  
+
   private void clearAxis(Layer layer, Bounds bounds) {
     layer.save();
     layer.setFillColor(gssProperties.bgColor);
@@ -138,49 +144,65 @@ public class LegendAxisPanel extends AxisPanel {
     target.width = source.width;
   }
 
-  private static ZoomIntervals createDefaultZoomIntervals() {
+  private static ZoomIntervals createDefaultZoomIntervals(XYPlot plot) {
     ZoomIntervals zooms = new ZoomIntervals();
-    
-    zooms.add(new ZoomInterval("1d", TimeUnit.DAY.ms()));
-    zooms.add(new ZoomInterval("5d", TimeUnit.DAY.ms() * 5));
-    zooms.add(new ZoomInterval("1m", TimeUnit.MONTH.ms()));
-    zooms.add(new ZoomInterval("3m", TimeUnit.MONTH.ms() * 3));
-    zooms.add(new ZoomInterval("6m", TimeUnit.MONTH.ms() * 6));
-    zooms.add(new ZoomInterval("1y", TimeUnit.YEAR.ms()));
-    zooms.add(new ZoomInterval("5y", TimeUnit.YEAR.ms() * 5));
-    zooms.add(new ZoomInterval("10y", TimeUnit.DECADE.ms()));
-    zooms.add(new ZoomInterval("100y", TimeUnit.CENTURY.ms()));
-    zooms.add(new ZoomInterval("1000y", TimeUnit.MILLENIUM.ms()));
-    zooms.add(new ZoomInterval("max", Double.MAX_VALUE).filterExempt(true));
-    
+
+    boolean isDateDomain = plot
+        .getDomainAxisRenderer() instanceof DomainAxisPanel;
+
+    if (isDateDomain) {
+      zooms.add(new ZoomInterval("1d", TimeUnit.DAY.ms()));
+      zooms.add(new ZoomInterval("5d", TimeUnit.DAY.ms() * 5));
+      zooms.add(new ZoomInterval("1m", TimeUnit.MONTH.ms()));
+      zooms.add(new ZoomInterval("3m", TimeUnit.MONTH.ms() * 3));
+      zooms.add(new ZoomInterval("6m", TimeUnit.MONTH.ms() * 6));
+      zooms.add(new ZoomInterval("1y", TimeUnit.YEAR.ms()));
+      zooms.add(new ZoomInterval("5y", TimeUnit.YEAR.ms() * 5));
+      zooms.add(new ZoomInterval("10y", TimeUnit.DECADE.ms()));
+      zooms.add(new ZoomInterval("100y", TimeUnit.CENTURY.ms()));
+      zooms.add(new ZoomInterval("1000y", TimeUnit.MILLENIUM.ms()));
+      zooms.add(new ZoomInterval("max", Double.MAX_VALUE).filterExempt(true));
+    } else {
+      double beginDomain = plot.getDatasets().getMinDomain();
+      double endDomain = plot.getDatasets().getMaxDomain();
+      int startPower = MathUtil.roundToNearestPowerOfTen(beginDomain);
+      int endPower = MathUtil.roundToNearestPowerOfTen(endDomain);
+
+      while (startPower <= endPower) {
+        zooms.add(new ZoomInterval("" + startPower / 2, startPower / 2));
+        zooms.add(new ZoomInterval("" + startPower, startPower));
+        startPower *= 10;
+      }
+      zooms.add(new ZoomInterval("max", Double.MAX_VALUE).filterExempt(true));
+    }
     return zooms;
   }
-  
+
   /**
-   * Currently, this method naively lays out the ZoomPanel and
-   * DateRangePanel on the X-axis.  Ultimately, layout rules and 
-   * heuristics will be split out into a separate LayoutStrategy 
-   * interface of some sort.
+   * Currently, this method naively lays out the ZoomPanel and DateRangePanel on
+   * the X-axis.  Ultimately, layout rules and heuristics will be split out into
+   * a separate LayoutStrategy interface of some sort.
    */
   private void layoutPanels(Bounds parentBounds) {
     // The minimum distance allowed between the zoom panel and the dataset 
     // legend panel.
     final int minCushion = 3;
-    
+
     double parentWidth = parentBounds.width;
-    
+
     dateRangePanel.resizeToIdealWidth();
     zoomPanel.resizeToIdealWidth();
     zoomPanel.show(true);
     double idealZoomPanelWidth = zoomPanel.getWidth();
-    
+
     // First, see if the panels in their prettiest form will 
     //fit within the container's bounds
-    double cushion = parentWidth - zoomPanel.getWidth() - dateRangePanel.getWidth();
+    double cushion = parentWidth - zoomPanel.getWidth() - dateRangePanel
+        .getWidth();
     if (cushion >= minCushion) {
       return;
     }
-    
+
     // Doesn't fit? Then compress only the date range panel
     dateRangePanel.resizeToMinimalWidth();
     topRightJustify(dateRangePanel, parentBounds);
@@ -188,7 +210,7 @@ public class LegendAxisPanel extends AxisPanel {
     if (cushion >= minCushion) {
       return;
     }
-    
+
     // Still doesn't fit? Then compress only the zoom link panel
     zoomPanel.resizeToMinimalWidth();
     dateRangePanel.resizeToIdealWidth();
@@ -197,7 +219,7 @@ public class LegendAxisPanel extends AxisPanel {
     if (cushion >= minCushion) {
       return;
     }
-    
+
     // Still doesn't fit? Then compress both panels
     dateRangePanel.resizeToMinimalWidth();
     topRightJustify(dateRangePanel, parentBounds);
@@ -205,24 +227,26 @@ public class LegendAxisPanel extends AxisPanel {
     if (cushion >= minCushion) {
       return;
     }
-    
+
     // Still doesn't fit? Then hide the zoom links completely.
     zoomPanel.show(false);
   }
-  
+
   /**
-   * Positions the specified panel in the top-right corner of the specified bounds.
+   * Positions the specified panel in the top-right corner of the specified
+   * bounds.
    */
   private void topRightJustify(Panel panel, Bounds parentBounds) {
-    panel.setLocation(parentBounds.rightX() - panel.getWidth() - 2, parentBounds.y);
+    panel.setLocation(parentBounds.rightX() - panel.getWidth() - 2,
+        parentBounds.y);
   }
-  
+
   /**
    * For debugging purposes
    */
   private static void hiliteBounds(Bounds b, Layer layer) {
     layer.save();
-   
+
     layer.setLayerOrder(1);
     //layer.setTransparency(.35f);
     layer.setFillColor("#50D0FF");
@@ -230,5 +254,4 @@ public class LegendAxisPanel extends AxisPanel {
 
     layer.restore();
   }
-  
 }
