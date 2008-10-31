66aa7d4fd4e99ed8d836c1148c3f73eba66ba117
cromwellian <cromwellian@5cd50543-e33d-0410-8f10-d7e7bb9d3266>
2008-10-31 05:09:18 +0000

    Extra fields and options to speed up android performance.

--- chronoscope/src/main/java/org/timepedia/chronoscope/client/canvas/Color.java
+++ chronoscope/src/main/java/org/timepedia/chronoscope/client/canvas/Color.java
@@ -7,9 +7,26 @@ public class Color implements PaintStyle {
 
   private final String color;
 
+  private boolean rgbaSet = false;
+
+  public boolean isRgbaSet() {
+    return rgbaSet;
+  }
+
+  public void setRgba(int rgba) {
+    this.rgba = rgba;
+    rgbaSet = true;
+  }
+
+  private int rgba;
+
   public Color(String color) {
 
     this.color = color;
+  }
+
+  public int getRGBA() {
+    return this.rgba;
   }
 
   public String toString() {
