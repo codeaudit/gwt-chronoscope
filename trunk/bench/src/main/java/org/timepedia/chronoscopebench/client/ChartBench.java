3104b48b8de11bfbc235aa586d608f92490cf9dc
chadtakahashi <chadtakahashi@5cd50543-e33d-0410-8f10-d7e7bb9d3266>
2008-11-11 05:34:07 +0000

    - Fixed bug in ChartBench in which subsequent presses of the 'Bench' button did not reset the plot domain, resulting in deceptively fast rendering times and FPS's. - Restructured code in ChartBench.benchmark.execute()

--- trunk/bench/src/main/java/org/timepedia/chronoscopebench/client/ChartBench.java
+++ trunk/bench/src/main/java/org/timepedia/chronoscopebench/client/ChartBench.java
@@ -14,9 +14,11 @@ import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.Widget;
 
 import org.timepedia.chronoscope.client.About;
-import org.timepedia.chronoscope.client.Dataset;
-import org.timepedia.chronoscope.client.Overlay;
 import org.timepedia.chronoscope.client.ChronoscopeOptions;
+import org.timepedia.chronoscope.client.Dataset;
+import org.timepedia.chronoscope.client.Datasets;
+import org.timepedia.chronoscope.client.Overlay;
+import org.timepedia.chronoscope.client.XYPlot;
 import org.timepedia.chronoscope.client.browser.ChartPanel;
 import org.timepedia.chronoscope.client.browser.Chronoscope;
 import org.timepedia.chronoscope.client.browser.JSONDataset;
@@ -90,66 +92,73 @@ public class ChartBench implements EntryPoint {
   }
 
   private void benchMark(final View view) {
-    final double dO = view.getChart().getPlot().getDomain().getStart();
-    final double cD = view.getChart().getPlot().getDomain().length();
+    final XYPlot plot = view.getChart().getPlot();
+    final Datasets datasets = plot.getDatasets();
+    
+    // Reset plot domain to 'max zoom'
+    //plot.getDomain().setEndpoints(datasets.getMinDomain(),
+    //    datasets.getMaxDomain());
+    
+    final double dO = plot.getDomain().getStart();
+    final double cD = plot.getDomain().length();
     final double dC = dO + cD / 2;
     final int lim = GWT.isScript() ? 100 : 5;
     final int numTrials = 10;
-    final double trials[] = new double[numTrials];
+    final double trialTimes[] = new double[numTrials];
+    
     if (benchTable == null) {
       benchTable = new FlexTable();
       RootPanel.get().add(benchTable);
     }
+    
     benchTable.clear();
     benchTable.getFlexCellFormatter().setColSpan(0, 0, 2);
     benchTable
         .setWidget(0, 0, new HTML("Trial Results (Trials=" + numTrials + ")"));
+    
     DeferredCommand.addCommand(new IncrementalCommand() {
       int trialNum = 0;
-
       int frameNum = 0;
-
       double curTime = 0;
-
+      boolean doContinue = false;
+      
       public boolean execute() {
 
-        if (trialNum < numTrials && frameNum < lim) {
-          double ncd = cD - cD / 1.5 * ((double) frameNum / lim);
-          double ndo = dC - ncd / 2;
-          view.getChart().getPlot().getDomain().setEndpoints(ndo, ndo + ncd);
-          double start = Duration.currentTimeMillis();
-          view.getChart().redraw();
-          double end = Duration.currentTimeMillis();
-          curTime += end - start;
-          frameNum++;
-          return true;
-        }
-        frameNum = 0;
         if (trialNum < numTrials) {
-
-          trials[trialNum] = curTime;
-          frameNum = 0;
-          trialNum++;
-          benchTable.setWidget(trialNum, 0, new HTML("Trial " + trialNum));
-          benchTable.setWidget(trialNum, 1, new HTML(curTime + "ms"));
-          if(trialNum < numTrials) curTime = 0;
-
-          return true;
-        } else {
-          benchTable.setWidget(trialNum, 0, new HTML("Trial " + trialNum));
-          benchTable.setWidget(trialNum, 1, new HTML(curTime + "ms"));
-          Arrays.sort(trials);
-          double mean = 0;
-          for (int i = 1; i < trials.length - 1; i++) {
-            mean += trials[i];
+          if (frameNum < lim) {
+            double ncd = cD - cD / 1.5 * ((double) frameNum / lim);
+            double ndo = dC - ncd / 2;
+            plot.getDomain().setEndpoints(ndo, ndo + ncd);
+            double start = Duration.currentTimeMillis();
+            view.getChart().redraw();
+            double end = Duration.currentTimeMillis();
+            curTime += end - start;
+            frameNum++;
           }
-          mean /= trials.length - 2;
+          else {
+            trialTimes[trialNum] = curTime;
+            trialNum++;
+            benchTable.setWidget(trialNum, 0, new HTML("Trial " + trialNum));
+            benchTable.setWidget(trialNum, 1, new HTML(curTime + " ms"));
+            frameNum = 0;
+            curTime = 0;
+          }
+          
+          doContinue = true;
+        }
+        else {
+          Arrays.sort(trialTimes);
+          double mean = 0;
+          for (int i = 1; i < trialTimes.length - 1; i++) {
+            mean += trialTimes[i];
+          }
+          mean /= trialTimes.length - 2;
           double stddev = 0;
-          for (int i = 1; i < trials.length - 1; i++) {
-            double x = trials[i] - mean;
+          for (int i = 1; i < trialTimes.length - 1; i++) {
+            double x = trialTimes[i] - mean;
             stddev += x * x;
           }
-          stddev /= trials.length - 2;
+          stddev /= trialTimes.length - 2;
           stddev = Math.sqrt(stddev);
           benchTable.setWidget(trialNum + 1, 0, new HTML("Summary"));
           benchTable.setWidget(trialNum + 1, 1,
@@ -163,8 +172,11 @@ public class ChartBench implements EntryPoint {
                   + About.getRevision() + "&mean=" + mean + "&stddev="
                   + stddev);
           RootPanel.get().add(report);
-          return false;
+          
+          doContinue = false;
         }
+        
+        return doContinue;
       }
     });
   }
