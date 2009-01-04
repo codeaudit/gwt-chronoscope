61be37208c9f218b75b1374b84b64d6a507f58fa
cromwellian <cromwellian@5cd50543-e33d-0410-8f10-d7e7bb9d3266>
2009-01-04 07:43:46 +0000

    Added Dataset.getLowestMipMapForInterval Refactored implementation from XYPlotRenderer to AbstractDataset Lays groundwork for new sharding system.

--- chronoscope/src/main/java/org/timepedia/chronoscope/client/data/AbstractDataset.java
+++ chronoscope/src/main/java/org/timepedia/chronoscope/client/data/AbstractDataset.java
@@ -2,12 +2,14 @@ package org.timepedia.chronoscope.client.data;
 
 import org.timepedia.chronoscope.client.Dataset;
 import org.timepedia.chronoscope.client.data.tuple.Tuple2D;
+import org.timepedia.chronoscope.client.util.Array1D;
 import org.timepedia.chronoscope.client.util.Interval;
+import org.timepedia.chronoscope.client.util.Util;
 
 /**
- * Provides skeletal implementation of an {@link Dataset} to simplify implementation
- * of a concrete class.
- * 
+ * Provides skeletal implementation of an {@link Dataset} to simplify
+ * implementation of a concrete class.
+ *
  * @author Chad Takahashi
  */
 public abstract class AbstractDataset<T extends Tuple2D> implements Dataset<T> {
@@ -15,13 +17,13 @@ public abstract class AbstractDataset<T extends Tuple2D> implements Dataset<T> {
   protected double minDomainInterval;
 
   protected String identifier, rangeLabel;
-  
+
   protected String preferredRenderer;
-  
+
   protected Interval preferredRangeAxisInterval;
-  
+
   private Interval domainExtrema = new Interval(0.0, 0.0);
-  
+
   public final Interval getDomainExtrema() {
     this.domainExtrema.setEndpoints(getX(0), getX(getNumSamples() - 1));
     return this.domainExtrema;
@@ -34,11 +36,28 @@ public abstract class AbstractDataset<T extends Tuple2D> implements Dataset<T> {
   public final double getMinDomainInterval() {
     return minDomainInterval;
   }
-  
+
+  public MipMapRegion getLowestMipMapForInterval(Interval region, int maxSamples) {
+    int domainStartIdx = 0;
+    int domainEndIdx = 0;
+    MipMapChain mipMapChain = getMipMapChain();
+    MipMap bestMipMap = mipMapChain.getMipMap(0);
+    while (true) {
+      Array1D domain = bestMipMap.getDomain();
+      domainStartIdx = Util.binarySearch(domain, region.getStart());
+      domainEndIdx = Util.binarySearch(domain, region.getEnd());
+      if ((domainEndIdx - domainStartIdx) <= maxSamples) {
+        break;
+      }
+      bestMipMap = bestMipMap.next();
+    }
+    return new MipMapRegion(bestMipMap, domainStartIdx, domainEndIdx);
+  }
+
   public final Interval getPreferredRangeAxisInterval() {
     return preferredRangeAxisInterval;
   }
-  
+
   public final String getPreferredRenderer() {
     return preferredRenderer;
   }
