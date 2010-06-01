18db4a7a7159346adecda0b86bb9e1dec8bd77b4
cromwellian <cromwellian@5cd50543-e33d-0410-8f10-d7e7bb9d3266>
2010-06-01 12:45:01 +0000

    Rudimentary incremental dataset support

---
+++ trunk/chronoscope-api/src/main/java/org/timepedia/chronoscope/client/data/IncrementalDataResponse.java
@@ -0,0 +1,16 @@
+package org.timepedia.chronoscope.client.data;
+
+import com.google.gwt.core.client.JsArrayNumber;
+
+import org.timepedia.exporter.client.Export;
+import org.timepedia.exporter.client.ExportPackage;
+import org.timepedia.exporter.client.Exportable;
+
+/**
+ * Called by client code with incremental data.
+ */
+@ExportPackage("chronoscope")
+@Export
+public interface IncrementalDataResponse extends Exportable {
+  void addData(JsArrayNumber domain, JsArrayNumber range);
+}
