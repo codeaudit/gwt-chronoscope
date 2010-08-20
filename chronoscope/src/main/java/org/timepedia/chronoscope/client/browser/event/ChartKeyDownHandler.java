f452129d2a311642e8e58f8003c383bf61f32ea7
timepedia <timepedia@5cd50543-e33d-0410-8f10-d7e7bb9d3266>
2010-08-20 18:23:20 +0000

    updating to use KeyCodes, TBD: arrow key bubbling while focused

--- chronoscope/src/main/java/org/timepedia/chronoscope/client/browser/event/ChartKeyDownHandler.java
+++ chronoscope/src/main/java/org/timepedia/chronoscope/client/browser/event/ChartKeyDownHandler.java
@@ -1,9 +1,9 @@
 package org.timepedia.chronoscope.client.browser.event;
 
+import com.google.gwt.event.dom.client.KeyCodes;
 import com.google.gwt.event.dom.client.KeyDownEvent;
 import com.google.gwt.event.dom.client.KeyDownHandler;
 import com.google.gwt.user.client.Event;
-import com.google.gwt.user.client.ui.KeyboardListener;
 
 /**
  * Handles the event where the user depresses a key.
@@ -16,13 +16,13 @@ public class ChartKeyDownHandler extends AbstractEventHandler<KeyDownHandler>
 
   public void onKeyDown(KeyDownEvent event) {
     ChartState chartInfo = getChartState(event);
-    boolean handled = false;
+    boolean handled = true;
     int keyCode = event.getNativeKeyCode();
     // all checks (except TAB) are to prevent focus stealing by the browser.
-    if (keyCode == KeyboardListener.KEY_PAGEUP
-        || keyCode == KeyboardListener.KEY_PAGEDOWN
-        || keyCode == KeyboardListener.KEY_UP
-        || keyCode == KeyboardListener.KEY_DOWN || keyCode == KEY_TAB) {
+    if (keyCode == KeyCodes.KEY_PAGEUP
+        || keyCode == KeyCodes.KEY_PAGEDOWN
+        || keyCode == KeyCodes.KEY_UP
+        || keyCode == KeyCodes.KEY_DOWN || keyCode == KeyCodes.KEY_TAB) {
       handled = handleTabKey((Event) event.getNativeEvent(), chartInfo, keyCode,
           event.isShiftKeyDown());
     }
