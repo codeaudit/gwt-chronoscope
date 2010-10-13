383ac455b4b724f18a66ecdc8cd22695b80ae9d0
manuel.carrasco.m <manuel.carrasco.m@5cd50543-e33d-0410-8f10-d7e7bb9d3266>
2010-10-13 00:04:08 +0000

    example to test setTimeZoneOffsets in devmode

--- trunk/samples/src/main/java/org/timepedia/chronoscopesamples/public/prepare_large_datasets.pl
+++ trunk/samples/src/main/java/org/timepedia/chronoscopesamples/public/prepare_large_datasets.pl
@@ -1,12 +1,13 @@
 #!/usr/bin/perl
 
-my $n = 15;
+my $n = 340;
 # put new lines in one-line datasets files 
 # remove all data from each serie but the $n first values
 while(<>) {
     chomp;
     s/([^:]+:[^:]+,)/$1\n/g;
     s/(\:\[)((.+?,){$n})([^\],]+\])/$1$3]/g;
+    s/,+\]/\]/g;
     print "$_\n";
 }
 
