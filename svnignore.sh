a15d70174f443679aa1b1b40a3573bc412325540
manuel.carrasco.m <manuel.carrasco.m@5cd50543-e33d-0410-8f10-d7e7bb9d3266>
2010-11-15 18:01:27 +0000

    updating svnignore properties

--- svnignore.sh
+++ svnignore.sh
@@ -1,3 +1,21 @@
-svn propset svn:ignore --recursive -F .svnignore .
-echo "current properties:"
-svn propget svn:ignore .
+## Just a script to add folders/files to svnignore
+
+[ -z "$1" ] && echo "Usage: $0 <list of file/folder names to ignore>" && exit
+
+p=`pwd`
+
+while [ -n "$1" ]
+do
+   for i in `find . -name $1`
+   do
+      d=`dirname $i`
+      cd $p
+      cd $d
+      svn propget svn:ignore . > /tmp/$$
+      echo $1 >> /tmp/$$
+      svn propset svn:ignore -F /tmp/$$ .
+   done
+   shift
+done
+
+
